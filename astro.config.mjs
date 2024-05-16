0e5c1d03c4d3ef6be6f674add9335217db0ea388
Haydon Ge <haydonge@gmail.com>
2024-05-16 21:49:45 +0800

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind(), db()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel()
 });
