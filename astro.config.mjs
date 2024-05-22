681b0f73123552c10c88219cded87b03d543a49d
haydonge <haydonge@gmail.com>
2024-05-22 13:27:07 +0800

    update index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
 import vercel from "@astrojs/vercel/serverless";
-
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), db(), react()],
+  integrations: [icon(), tailwind(), db()],
   output: "server",
   adapter: vercel()
 });
